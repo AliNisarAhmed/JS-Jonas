770f9e1d5cfd8bfafe82b83b0ee5f983298fee9f
AliNisarAhmed <ali.nisar87@gmail.com>
2018-06-05 21:48:51 +0500

    add functionality

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -10,12 +10,11 @@ GAME RULES:
 */
 
 const scores = [0, 0];
-const roundScore = 0;
-const activePlayer = 0;
+let roundScore = 0;
+let activePlayer = 0;
 
 // const dice = Math.floor(Math.random() * 6 + 1);
 
-// document.querySelector(`#current-${activePlayer}`).textContent = dice;
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
@@ -28,10 +27,30 @@ document.querySelector('.dice').style.display = "none";
 document.querySelector('.btn-roll').addEventListener('click', () => {
   let dice = Math.floor(Math.random() * 6 + 1);
 
-  let diceDOM = document.querySelector('.dice');
 
+  // Display the dice roll result - update image
+  let diceDOM = document.querySelector('.dice');
   diceDOM.style.display = 'block';
   diceDOM.src = `dice-${dice}.png`;
+
+  //update round score and add to current score
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+  } else {
+    activePlayer = activePlayer === 0 ? 1: 0;
+    roundScore = 0;
+    
+    //when 1 appears, make the current score of activePlayer to zero
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    // change active player class
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = "none";
+  }
 })
 
 
