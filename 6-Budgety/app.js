91ca4c2f84bebb54aefd3bab0364d95f712cb290
AliNisarAhmed <ali.nisar87@gmail.com>
2018-06-10 12:48:20 +0500

    add budget folder

---
+++ 6-Budgety/app.js
@@ -0,0 +1,78 @@
+
+// ====== BUDGET Controller ====== //
+
+const budgetController = (function () {
+ 
+})();
+
+
+
+
+// ====== UI Controller ====== //
+
+const UIController = (function() {
+
+  const DOMStrings = {
+    inputType : '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  }
+
+  return {
+    getInput : function () {
+      const type = document.querySelector(DOMStrings.inputType).value; // will be either inc or exp
+
+      const description = document.querySelector(DOMStrings.inputDescription).value;
+
+      const value = document.querySelector(DOMStrings.inputValue).value;
+
+      return {
+        type: type,
+        description: description,
+        value: value
+      };
+    },
+
+    getDOMStrings: function() {
+      return DOMStrings;
+    },
+
+  };
+
+})();
+
+
+
+// ====== APP Controller ====== //
+
+const controller = (function(bdgtCntrl, UICntrl) {
+
+  const DOM = UICntrl.getDOMStrings();
+
+  const ctrlAddItem = function () {
+
+    // 1. Get the field input data
+
+    const input = UICntrl.getInput();
+    console.log(input);
+
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the Budget
+
+    // 5. Display the budget in the UI
+  }
+  
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', (event) => {
+    if(event.key === "Enter") {
+      ctrlAddItem();
+    }
+  });
+
+})(budgetController, UIController);
